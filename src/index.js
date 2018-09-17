258dc2d974f5afb8a21e35a7fab13c2a58da8283
Kishkun <durata55@gmail.com>
2018-09-17 18:52:35 +0300

    refactoring sorter in index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,82 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.array = [];
+        this.meaning = false;
+        Sorter.prototype.sortArray = function (left, right) {
+            return 0;
+        };
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this.array.push(element);
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.array[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this.array.length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this.array
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
+        if (this.meaning) {
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+            indices = indices.sort();
+
+            const left = indices[0];
+
+            const right = indices[1];
+
+            if (this.sortArray(this.array[left], this.array[right]) > 0) {
+
+                const a = this.array[left];
+
+                this.array[left] = this.array[right];
+
+                this.array[right] = a;
+
+            }
+
+        } else {
+
+            if (Array.isArray(indices)) {
+
+                indices = indices.sort();
+
+                for (let i = 0, endI = indices.length - 1; i < endI; i++) {
+
+                    for (let j = 0, endJ = endI - i; j < endJ; j++) {
+
+                        if (this.array[indices[j]] > this.array[indices[j + 1]]) {
+
+                            let b = this.array[indices[j]];
+
+                            this.array[indices[j]] = this.array[indices[j + 1]];
+
+                            this.array[indices[j + 1]] = b;
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+    }
+
+    setComparator(func1, arg) {
+
+        Sorter.prototype.sortArray = func1;
+
+        this.meaning = true;
+    }
 }
 
 module.exports = Sorter;
